555e5bddfa0f0c5e2b5e313ace306b77dba351b8
Gustavo Seoane <gseoane87@gmail.com>
2022-06-04 12:59:14 -0300

    Index + Base + Herencia

--- Abreu/urls.py
+++ Abreu/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from Abreu.views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', index, name = 'index'),
     path('tienda/',include('Tienda.urls'))
 ]
