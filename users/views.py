b1f59e28f0e5ba6bc084e13d8d14925ed70e59ef
Ismael Abero <iafemprende@gmail.com>
2022-06-26 20:09:46 -0300

    login,logout,register funcionando

--- users/views.py
+++ users/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
 from django.http import HttpResponse
-from django.contrib.auth.forms import AuthenticationForm
-from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth import authenticate, login, logout
+
+from users.forms import User_registration_form
 
 # Create your views here.
 
@@ -31,4 +33,28 @@ def login_view(request):
         context = {'form':form}
         return render(request, 'auth/login.html',context=context)
 
+def logout_view(request):
+    logout(request)
+    return redirect('index')
+
+def register_view(request):
+    if request.method == 'POST':
+        form = User_registration_form(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username = username, password = password)
+            login(request, user)
+            context = {'message':f'Usuario creado satisfactoriamente. Bienvenido {username}'}
+            return render(request, 'index.html',context = context)
+        else:
+            errors = form.errors
+            form = User_registration_form()
+            context = {'errors':errors, 'form':form}
+            return render(request,'auth/register.html',context = context)
+    else:
+        form = User_registration_form()
+        context = {'form':form}
+        return render(request,'auth/register.html',context = context)
 
