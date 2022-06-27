89c2b27be5b6dadc9379e8abcc037b0a81a9aa57
Ismael Abero <iafemprende@gmail.com>
2022-06-26 22:12:19 -0300

    login con foto y nombre en navbar

--- Abreu/views.py
+++ Abreu/views.py
@@ -3,4 +3,5 @@ from django.shortcuts import render
 def index(request):
     print(request.user)
     print(request.user.is_authenticated)
+    print(request.user.user_profile.phone)
     return render(request, 'index.html')
--- users/models.py
+++ users/models.py
@@ -1,3 +1,22 @@
+from distutils.command.upload import upload
 from django.db import models
 
+from django.contrib.auth.models import User
+
 # Create your models here.
+
+class User_profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='user_profile')
+    phone = models.CharField(max_length=20)
+    profile_image = models.ImageField(upload_to='profile_image')
+
+    class Meta:
+      verbose_name = 'perfil'
+      verbose_name_plural = 'perfiles'
+
+#    def __str__(self):
+#        return self.user
+
+
+
+
